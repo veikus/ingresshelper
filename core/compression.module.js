ca03bf496a67e7b2b05c68bb1573d8585ef87386
aerocaterpillar <volgen@gmail.com>
2015-07-24 00:41:43 +0800

    Update compression.module.js
    zh-cmn-Hans is simplified chinese（簡體中文）, zh-cmn-Hant is traditional chinese（繁體中文）

--- core/compression.module.js
+++ core/compression.module.js
@@ -54,29 +54,40 @@
         en: 'You can get uncompressed images (increase data transfer)',
         ru: 'Вы можете отключить сжатие изображений (требуется больше трафика)',
         ua: 'Ви можете вимкнути стиснення зображень (потребує більше трафіку)'
+        zh-cmn-Hans: '你可以收到未经压缩的图片（会增加传输的数据）'
+        zh-cmn-Hant: '你可以收到未經壓縮的圖片（會增加傳輸的數據）'
     };
+        
 
     onText = {
         en: 'Enable compression',
         ru: 'Включить сжатие',
         ua: 'Увімкнути стиснення'
+        zh-cmn-Hans: '启用压缩'
+        zh-cmn-Hant: '啟用壓縮'
     };
 
     offText = {
         en:'Disable compression',
         ru: 'Отключить сжатие',
         ua: 'Вимкнути стиснення'
+        zh-cmn-Hans: '停用压缩'
+        zh-cmn-Hant: '停用壓縮'
     };
 
     savedText = {
         en: 'Changes saved',
         ru: 'Изменения сохранены',
         ua: 'Зміни збережено'
+        zh-cmn-Hans: '修改已保存'
+        zh-cmn-Hant: '修改已保存'
     };
 
     incorrectOptionText = {
         en: 'Incorrect input. Please try again',
         ru: 'Неправильный выбор. Выберите из предложенных вариантов',
         ua: 'Неправильний вибір. Виберіть із запропонованих варіантів'
+        zh-cmn-Hans: '输入有误，请重试'
+        zh-cmn-Hant: '輸入有誤，請重試'        
     };
 }());

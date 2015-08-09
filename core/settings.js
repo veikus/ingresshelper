e06927fedb2c655c98160a278b16f26b094d47bf
Artem Veikus <artem@veikus.com>
2015-08-09 16:26:11 +0300

    Remove closure

--- core/settings.js
+++ core/settings.js
@@ -3,83 +3,81 @@
  * @author Artem Veikus artem@veikus.com
  * @version 3.0
  */
-(function() {
-    var localStorage = {
-        getItem: function() {
-            return undefined;
-        },
+var localStorage = {
+    getItem: function () {
+        return undefined;
+    },
 
-        setItem: function() {
-            return true;
-        }
-    };
+    setItem: function () {
+        return true;
+    }
+};
 
-    /**
-     * Chat settings
-     * @param id {Number} Chat id
-     * @param lang {String} Set user language
-     * @returns {String|null} Current language code (or null if not defined)
-     */
-    module.exports.lang = function(id, lang) {
-        var settings = localStorage.getItem('settings__chat_' + id);
+/**
+ * Chat settings
+ * @param id {Number} Chat id
+ * @param lang {String} Set user language
+ * @returns {String|null} Current language code (or null if not defined)
+ */
+module.exports.lang = function (id, lang) {
+    var settings = localStorage.getItem('settings__chat_' + id);
 
-        if (settings) {
-            settings = JSON.parse(settings);
-        } else {
-            settings = {};
-        }
+    if (settings) {
+        settings = JSON.parse(settings);
+    } else {
+        settings = {};
+    }
 
-        if (lang) {
-            settings.language = lang;
-            localStorage.setItem('settings__chat_' + id, JSON.stringify(settings));
-        }
+    if (lang) {
+        settings.language = lang;
+        localStorage.setItem('settings__chat_' + id, JSON.stringify(settings));
+    }
 
-        return settings && settings.language || null;
-    };
+    return settings && settings.language || null;
+};
 
-    /**
-     * Compression settings
-     * @param id {Number} Chat id
-     * @param value {Boolean} Set compression value
-     * @returns {Boolean} Value of compression setting
-     */
-    module.exports.compression = function(id, value) {
-        var settings = localStorage.getItem('settings__chat_' + id);
+/**
+ * Compression settings
+ * @param id {Number} Chat id
+ * @param value {Boolean} Set compression value
+ * @returns {Boolean} Value of compression setting
+ */
+module.exports.compression = function (id, value) {
+    var settings = localStorage.getItem('settings__chat_' + id);
 
-        if (settings) {
-            settings = JSON.parse(settings);
-        } else {
-            settings = {};
-        }
+    if (settings) {
+        settings = JSON.parse(settings);
+    } else {
+        settings = {};
+    }
 
-        if (typeof (value) === 'boolean') {
-            settings.compression = value;
-            localStorage.setItem('settings__chat_' + id, JSON.stringify(settings));
-        }
+    if (typeof (value) === 'boolean') {
+        settings.compression = value;
+        localStorage.setItem('settings__chat_' + id, JSON.stringify(settings));
+    }
 
-        return settings && settings.hasOwnProperty('compression') ? settings.compression : true;
-    };
+    return settings && settings.hasOwnProperty('compression') ? settings.compression : true;
+};
 
-    /**
-     * Plugins settings
-     * @param id {Number} Chat id
-     * @param value {Array} Set new plugins list
-     * @returns {Array} Enabled plugins list
-     */
-    module.exports.plugins = function(id, value) {
-        var settings = localStorage.getItem('settings__chat_' + id);
+/**
+ * Plugins settings
+ * @param id {Number} Chat id
+ * @param value {Array} Set new plugins list
+ * @returns {Array} Enabled plugins list
+ */
+module.exports.plugins = function (id, value) {
+    var settings = localStorage.getItem('settings__chat_' + id);
 
-        if (settings) {
-            settings = JSON.parse(settings);
-        } else {
-            settings = {};
-        }
+    if (settings) {
+        settings = JSON.parse(settings);
+    } else {
+        settings = {};
+    }
 
-        if (value) {
-            settings.plugins = value;
-            localStorage.setItem('settings__chat_' + id, JSON.stringify(settings));
-        }
+    if (value) {
+        settings.plugins = value;
+        localStorage.setItem('settings__chat_' + id, JSON.stringify(settings));
+    }
 
-        return settings && settings.plugins || [];
-    };
-}());
+    return settings && settings.plugins || [];
+};

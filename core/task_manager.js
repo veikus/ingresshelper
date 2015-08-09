2f7f25a3468bb8a355f1cfe1ac2b00170a046319
Artem Veikus <artem@veikus.com>
2015-08-09 21:05:29 +0300

    Remove closure

--- core/task_manager.js
+++ core/task_manager.js
@@ -3,108 +3,105 @@
  * @author Artem Veikus artem@veikus.com
  * @version 3.0
  */
-(function() {
-    var inProgress, tasks,
-        i18n = require(__dirname + '/i18n_extend.js'),
-        telegram = require(__dirname + '/telegram.js'),
-        settings = require(__dirname + '/settings.js'),
-        express = require('express'),
-        path = require('path'),
-        expressApp = express();
+var inProgress, tasks,
+    i18n = require(__dirname + '/i18n_extend.js'),
+    telegram = require(__dirname + '/telegram.js'),
+    settings = require(__dirname + '/settings.js'),
+    express = require('express'),
+    path = require('path'),
+    expressApp = express();
 
-    /**
-     * Add task to queue
-     * @param options {object} Task options
-     * @param callback {Function} Function that will be called after telegram sent response
-     */
-    module.exports.add = function(options, callback) {
-        options = JSON.parse(JSON.stringify(options)); // TODO: Find better way to clone objects
-        options.callback = callback;
-        tasks.push(options);
-        // TODO: Save task in DB
-    };
+/**
+ * Add task to queue
+ * @param options {object} Task options
+ * @param callback {Function} Function that will be called after telegram sent response
+ */
+module.exports.add = function (options, callback) {
+    options = JSON.parse(JSON.stringify(options)); // TODO: Find better way to clone objects
+    options.callback = callback;
+    tasks.push(options);
+    // TODO: Save task in DB
+};
 
-    /**
-     * Return task length count
-     * @returns {Number} Tasks count
-     */
-    module.exports.queueLength = function() {
-        var count = tasks ? tasks.length : 0;
+/**
+ * Return task length count
+ * @returns {Number} Tasks count
+ */
+module.exports.queueLength = function () {
+    var count = tasks ? tasks.length : 0;
 
-        if (inProgress) {
-            ++count;
-        }
+    if (inProgress) {
+        ++count;
+    }
 
-        return count;
-    };
+    return count;
+};
 
-    tasks = []; // TODO: Load tasks from DB
+tasks = []; // TODO: Load tasks from DB
 
-    // Local server to communicate with phantom
-    expressApp.use('/iitc', express.static(__dirname + '/../ice_based_server/iitc'));
-    expressApp.use('/client', express.static(__dirname + '/../ice_based_server/client'));
+// Local server to communicate with phantom
+expressApp.use('/iitc', express.static(__dirname + '/../ice_based_server/iitc'));
+expressApp.use('/client', express.static(__dirname + '/../ice_based_server/client'));
 
-    expressApp.get('/get-task', function(req, res) {
-        var plugins, lang, resp, latitude, longitude;
+expressApp.get('/get-task', function (req, res) {
+    var plugins, lang, resp, latitude, longitude;
 
-        res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-        if (inProgress) {
-            // TODO: Mark previous task as failed in DB
-            lang = settings.lang(inProgress.chat);
-            resp = i18n(lang, 'tasks', 'something_went_wrong');
-            telegram.sendMessage(inProgress.chat, resp, null);
-        }
+    if (inProgress) {
+        // TODO: Mark previous task as failed in DB
+        lang = settings.lang(inProgress.chat);
+        resp = i18n(lang, 'tasks', 'something_went_wrong');
+        telegram.sendMessage(inProgress.chat, resp, null);
+    }
 
-        inProgress = tasks.pop();
+    inProgress = tasks.pop();
 
-        // If no more tasks
-        if (!inProgress) {
-            res.sendStatus(204);
-            return;
-        }
+    // If no more tasks
+    if (!inProgress) {
+        res.sendStatus(204);
+        return;
+    }
 
-        plugins = settings.plugins(inProgress.chat);
-        plugins.forEach(function(val, k) {
-            plugins[k] = location.origin + '/' + val;
-        });
-        inProgress.plugins = plugins;
-
-
-        latitude = inProgress.location.latitude;
-        longitude = inProgress.location.longitude;
-        inProgress.url = 'https://www.ingress.com/intel?ll=' + latitude + ',' + longitude + '&z=' + inProgress.zoom;
-
-        inProgress.fileName = path.resolve(__dirname + '/../screenshots/task-' + new Date().getTime() + '.png');
-
-        if (inProgress.zoom <= 7) {
-            inProgress.timeout = 3 * 60 * 1000;
-        } else {
-            inProgress.timeout = 2 * 60 * 1000;
-        }
-
-        res.send(JSON.stringify(inProgress));
+    plugins = settings.plugins(inProgress.chat);
+    plugins.forEach(function (val, k) {
+        plugins[k] = location.origin + '/' + val;
     });
+    inProgress.plugins = plugins;
 
-    expressApp.get('/complete-task', function(req, res) {
-        var compression;
 
-        res.setHeader('Access-Control-Allow-Origin', '*');
+    latitude = inProgress.location.latitude;
+    longitude = inProgress.location.longitude;
+    inProgress.url = 'https://www.ingress.com/intel?ll=' + latitude + ',' + longitude + '&z=' + inProgress.zoom;
 
-        if (!inProgress) {
-            res.sendStatus(400);
-            return;
-        }
+    inProgress.fileName = path.resolve(__dirname + '/../screenshots/task-' + new Date().getTime() + '.png');
 
-        // TODO: Mark task as finished
+    if (inProgress.zoom <= 7) {
+        inProgress.timeout = 3 * 60 * 1000;
+    } else {
+        inProgress.timeout = 2 * 60 * 1000;
+    }
 
-        compression = settings.compression(inProgress.chat);
-        telegram.sendPhoto(inProgress.chat, inProgress.fileName, compression);
+    res.send(JSON.stringify(inProgress));
+});
 
-        inProgress = null;
-        res.sendStatus(200);
-    });
+expressApp.get('/complete-task', function (req, res) {
+    var compression;
 
-    expressApp.listen(9999);
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-}());
+    if (!inProgress) {
+        res.sendStatus(400);
+        return;
+    }
+
+    // TODO: Mark task as finished
+
+    compression = settings.compression(inProgress.chat);
+    telegram.sendPhoto(inProgress.chat, inProgress.fileName, compression);
+
+    inProgress = null;
+    res.sendStatus(200);
+});
+
+expressApp.listen(9999);
